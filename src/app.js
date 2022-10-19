d06ea65033c2322ea80c3c218e1b525dec52ba49
Rafael088 <jerafa088@gmail.com>
2022-10-18 21:14:06 -0500

    fix: rutas

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express  from "express";
 import users from "./routes/users.js";
 import pets from "./routes/pets.js";
-const app = express();
+import cors from 'cors';
 
+const app = express();
+app.use(cors());
 app.use(express.json())
 app.use(users)
 app.use(pets)
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import app from './app.js';
 import { sequelize } from "./db/db.js";
-import cors from 'cors';
 
 const port = 3001
-const optionsCors = {
-    origin: 'https://localhost:3000',
-    optionsSuccessStatus: 200
-}
-app.use(cors(optionsCors));
+
 async function main() {
     try {
         await sequelize.sync()
